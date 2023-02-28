1a34a2d2ee3e1e0ce4db5857e2ed8dcc4b081fa7
aleksandrostroushko <a.ostroushko@zebrains.tearm>
2023-02-28 19:07:26 +0300

    обновление файлов

--- www/index.php
+++ www/index.php
@@ -1,61 +1,8 @@
-<?php
-
-use Lepr\Patterns\Factory\PhoneFactory;
-use Lepr\Patterns\Prototype\Shape;
-use Lepr\Patterns\Singelton\Singelton;
-use Lepr\Patterns\Strategy\RedHeadDuck;
-
+<?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
+?>
 
-//Фабричный метод
-$cell = (new PhoneFactory)->createCellPhone();
-$smart = (new PhoneFactory)->createSmartPhone();
-$cell->call();
-echo '<br>';
-$smart->call();
-echo '<br>';
-echo '<br>';
+Пустая страница. <a href="/bitrix/admin/">Перейти в Панель Управления</a>.
 
-//Стратегия
-$redDuck = new RedHeadDuck();
-$redDuck->fly();
-echo '<br>';
-$redDuck->quack();
-echo '<br>';
-echo '<br>';
-
-//Сингелтон
-$singelton = new Singelton();
-$singelto1n = new Singelton();
-echo '<pre>';
-var_dump($singelton->getInstance());
-echo '</pre>';
-
-echo '<pre>';
-var_dump($singelto1n->getInstance());
-echo '</pre>';
-echo '<br>';
-$singelton->getInstance()->sayHello();
-echo '<br>';
-echo '<br>';
-
-//Прототип
-$shape = new Shape();
-$shape->setName('Цвет');
-$shape->setColor('Черный');
-
-$green = clone $shape;
-$green->setName('Цвет');
-$green->setColor('Зеленый');
-echo '<pre>';
-var_dump($green);
-echo '</pre>';
-echo '<pre>';
-var_dump($shape);
-echo '</pre>';
-echo '<br>';
-echo '<br>';
-
-
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
